24ae0919722386e1c1204ec84279b79e8eec873c
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-03-09 15:14:38 -0300

    Handling User Exceptions

---
+++ src/main/java/com/social/application/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.social.application.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
