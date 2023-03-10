8f92a5ab14fc30844310954a154beb7109f41a5c
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-03-10 17:53:38 -0300

    Getting a Post for Your ID - Implementation of Service and Controller

--- src/main/java/com/social/application/services/PostService.java
+++ src/main/java/com/social/application/services/PostService.java
@@ -1,12 +1,14 @@
 package com.social.application.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.social.application.model.Post;
 import com.social.application.repository.PostRepository;
+import com.social.application.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
@@ -16,6 +18,11 @@ public class PostService {
 	
 	public List<Post> findAll(){
 		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id Not Found."));
 	}
 	
 }
