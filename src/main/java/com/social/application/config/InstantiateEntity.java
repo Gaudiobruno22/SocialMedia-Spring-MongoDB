fa5097c42f5c0aa71149632c18c9d03ab7f7eddb
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-03-10 16:50:05 -0300

    Inserting DTO Methods on Post Class and Delivering References by Posts to Users

--- src/main/java/com/social/application/config/InstantiateEntity.java
+++ src/main/java/com/social/application/config/InstantiateEntity.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.social.application.dto.AuthorDTO;
 import com.social.application.model.Post;
 import com.social.application.model.User;
 import com.social.application.repository.PostRepository;
@@ -33,12 +34,13 @@ public class InstantiateEntity implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu ;viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu ;viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		maria.setPosts(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
-
 }
